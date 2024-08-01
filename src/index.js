f23149a28161938135f0fcae35889b73b2811f0e
Disha <sethidisha123@gmail.com>
2024-08-01 16:22:10 +0530

    completed publishing mail event

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 const express = require('express');
-
+const amqplib=require('amqplib');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const mailsender=require('./config/email-config')
 const app = express();
 
+const { EmailService}=require('./services')
+
+
+async function connectQueue(){
+    try {
+        const connection=await amqplib.connect("amqp://localhost");
+        const channel=await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue",async (data)=>{
+            // console.log(`${Buffer.from(data.content)}`);
+            const object=JSON.parse(`${Buffer.from(data.content)}`);
+          await  EmailService.sendEmail("duplicatedisha@gmail.com",object.recepientEmail,object.subject,object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+
+        
+    }
+}
 app.use(express.json());
+
 app.use(express.urlencoded({extended:true}));
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-try{
-    const response=await mailsender.sendMail({
-        from:ServerConfig.GMAIL_EMAIL,
-        to:'abc@gmail.com',
-        subject:'Is the service working?',
-        text:'Yes it is working'
-    });
-    console.log(response);
-}catch(error){
-    console.log(error);
-}
+ await connectQueue();
+ console.log("queue is up");
+    
 });
